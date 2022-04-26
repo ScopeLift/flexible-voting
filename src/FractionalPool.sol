0e626d5bacc788f418489f859de99283ae27ad18
Ben DiFrancesco <ben@scopelift.co>
2022-04-26 11:45:31 -0400

    WIP: some todos and planning for next steps

--- src/FractionalPool.sol
+++ src/FractionalPool.sol
@@ -10,6 +10,11 @@ contract FractionalPool {
 
     IVotingToken immutable public token;
 
+    // TODO: way of tracking weights
+    // Map depositor to deposit amount
+    // mapping (address => uint256) deposits;
+    // uint256 totalNetDeposits;
+
     constructor(IVotingToken _token) {
         token = _token;
         _token.delegate(address(this));
@@ -23,7 +28,15 @@ contract FractionalPool {
 
     // TODO: withdrawal method (update fractional voting power)
 
-    // TODO: express voting preference method
+    // TODO: express depositor voting preference method
+    /* NEXT:
+     *   - Test setup: Create proposal, propose it, advance to active state
+     *   - Pool: Mechanism for tracking a depositors current weight (just a mapping?)
+     *   - Test case: Depositor calls this new method, and is stored internally
+     *   - Test: eventually someone calls method to express this on governor contract
+     */
+
+     // TODO: Execute the total vote against the governor contract
 
     // TODO: "borrow", i.e. removes funds from the pool, but is not a withdrawal, i.e. not returning
     // funds to a user that deposited them. Ex: someone borrowing from a compound pool.
