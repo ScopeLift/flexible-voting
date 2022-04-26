3d34bbb3cc188461c1e6fee65cb93c9faf550eb6
Ben DiFrancesco <ben@scopelift.co>
2022-04-26 10:44:31 -0400

    Organize some TODOs for the fractional pool

--- src/FractionalPool.sol
+++ src/FractionalPool.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract FractionalPool {}
+contract FractionalPool {
+
+    // TODO: deposit method (update fractional voting power)
+
+    // TODO: withdrawal method (update fractional voting power)
+
+    // TODO: express voting preference method
+
+    // TODO: "borrow", i.e. removes funds from the pool, but is not a withdrawal, i.e. not returning
+    // funds to a user that deposited them. Ex: someone borrowing from a compound pool.
+}
