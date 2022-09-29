7e7c03fc5f61800fa6360d7270b1542d54a831db
David Laprade <david.laprade@gmail.com>
2022-09-29 17:17:31 -0400

    Downgrade solc so that we can import aave's OZ dependencies

--- test/GovToken.sol
+++ test/GovToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.8.10;
 
 import { ERC20Votes } from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import { ERC20Permit } from "openzeppelin-contracts/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
--- test/GovernorCountingFractional.t.sol
+++ test/GovernorCountingFractional.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.8.10;
 
 import { DSTestPlus } from "solmate/test/utils/DSTestPlus.sol";
 import { Vm } from "forge-std/Vm.sol";
