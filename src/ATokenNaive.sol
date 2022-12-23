73b752b054ad526f634f9d7471369415ab3014f3
David Laprade <david.laprade@gmail.com>
2022-12-23 13:57:15 -0500

    Add handleRepayment/2 to AToken mock for fork test compatibility

--- src/ATokenNaive.sol
+++ src/ATokenNaive.sol
@@ -275,6 +275,32 @@ contract ATokenNaive is AToken {
       IERC20(_underlyingAsset).safeTransfer(receiverOfUnderlying, amount);
     }
   }
+
+  function handleRepayment(
+    address user,
+    uint256 amount
+  ) external virtual onlyPool {
+    // We need this because the Aave code we compile is ahead of the Aave code deployed on
+    // Optimism (where our tests fork from).
+    //
+    // Currently on Optimism, AToken.handleRepayment is a 2-argument function, as seen in the
+    // existing AToken implementation:
+    //
+    //   https://optimistic.etherscan.io/address/0xa5ba6e5ec19a1bf23c857991c857db62b2aa187b#code
+    //
+    // But in the latest Aave v3 code, it is a 3-argument function:
+    //
+    //   https://github.com/aave/aave-v3-core/blob/c38c627683c0db0449b7c9ea2fbd68bde3f8dc01/contracts/protocol/tokenization/AToken.sol#L166-L170
+    //
+    // The change was made as a result of this issue:
+    //
+    //   https://github.com/aave/aave-v3-core/issues/742
+    //
+    // We expect that the on-chain AToken implementation will be upgraded to
+    // AToken.handleRepayment/3 at some point in the future. If/when that happens, we should remove
+    // this. But for now we need our aToken to have this function in our fork tests to maintain
+    // backwards compatibility.
+  }
   //===========================================================================
   // END: Aave overrides
   //===========================================================================
