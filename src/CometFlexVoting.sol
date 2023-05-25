c3b2fe3b9b19774d80ae83a0a2952579ea36312b
David Laprade <david.laprade@gmail.com>
2023-05-25 15:42:07 -0400

    Add FlexVotingComet description

--- src/CometFlexVoting.sol
+++ src/CometFlexVoting.sol
@@ -7,7 +7,35 @@ import {Checkpoints} from "@openzeppelin/contracts/utils/Checkpoints.sol";
 
 import {FlexVotingClient} from "src/FlexVotingClient.sol";
 
-// TODO add description
+/// @notice This is an extension of Compound V3's Comet contract which makes it
+/// possible for Comet token holders to still vote on governance proposals. This
+/// way, holders of governance tokens do not have to choose between earning
+/// yield on Compound and voting. They can do both.
+///
+/// This extension has the following requirements:
+///   (a) the base token be a governance token
+///   (b) the base token's governor contract support flexible voting (see
+///       GovernorCountingFractional)
+///
+/// If these requirements are met, base token depositors can call
+/// `Comet.expressVote` to signal their preference on open governance proposals.
+/// When they do so, this extension records that preference with weight
+/// proportional to the users's Comet balance at the proposal snapshot.
+///
+/// At any point after voting preferences have been expressed, Comet's public
+/// `castVote` function may be called to roll up all internal voting records
+/// into a single delegated vote to the Governor contract -- a vote which
+/// specifies the exact For/Abstain/Against totals expressed by Comet holders.
+/// Votes can be rolled up and cast in this manner multiple times for a given
+/// proposal.
+///
+/// Participating in governance via Comet voting is completely optional. Users
+/// otherwise still supply, borrow, and hold tokens with Compound as usual.
+///
+/// The original Comet that this contract was developed against can be viewed
+/// here:
+///
+///   https://github.com/compound-finance/comet/blob/3780c06b4eaa80a8c78e0ff770a7e8a1518db75e/contracts/Comet.sol
 contract CometFlexVoting is Comet, FlexVotingClient {
   using Checkpoints for Checkpoints.History;
 
