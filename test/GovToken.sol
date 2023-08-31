b104f3166c720b557e60b4ef52bbf8c6f3ccf6e4
keating <keating.dev@protonmail.com>
2023-08-31 16:41:28 -0400

    Updates to the ci configuration

--- test/GovToken.sol
+++ test/GovToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10;
 
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
